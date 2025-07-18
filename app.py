8c9b1ccad4a17ffb8f9761da5dacd4f8a41519f1
Baldwinsure <105661110+Baldwinsure@users.noreply.github.com>
2025-07-18 03:22:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,23 @@ import pickle
 from tensorflow.keras.layers import SpatialDropout1D
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
-import requests
+import gdown
 
 def download_model_from_drive(drive_id, destination):
     if not os.path.exists(destination):
         print("Downloading model from Google Drive...")
-        url = f"https://drive.google.com/uc?export=download&id={drive_id}"
-        response = requests.get(url, stream=True)
-        with open(destination, "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
+        url = f"https://drive.google.com/uc?id={drive_id}&export=download"
+        gdown.download(url, destination, quiet=False)
         print("Download complete.")
 
 # Replace with your actual Drive file ID
 drive_file_id = "16F80tsiZb2rAhOL1aTzs4yso7NWPQKFu"
 model_file_path = "sentiment_model.h5"
 download_model_from_drive(drive_file_id, model_file_path)
+
 # Load model with custom object
 model = tf.keras.models.load_model(
-    "sentiment_model.h5",
+    model_file_path,
     custom_objects={"SpatialDropout1D": SpatialDropout1D}
 )
 
@@ -42,15 +39,9 @@ st.title("Sentiment Classifier (LSTM Model)")
 text_input = st.text_area("Enter a sentence to classify")
 
 if st.button("Predict"):
-    # Load model only when needed
-    with st.spinner("Loading model..."):
-        model = tf.keras.models.load_model("sentiment_model.h5")
-    
-    # Preprocess
     sequences = tokenizer.texts_to_sequences([text_input])
     padded = pad_sequences(sequences, maxlen=30)
 
-    # Predict
     prediction = model.predict(padded)
     predicted_index = np.argmax(prediction, axis=1)[0]
     predicted_label = label_encoder.inverse_transform([predicted_index])[0]
